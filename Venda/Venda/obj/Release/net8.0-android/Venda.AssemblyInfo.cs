255b8061ee4ba8b64c344f5ffb9b9e62b7228d51
PatinRaba <richarprestes@gmail.com>
2024-03-08 22:16:49 -0300

    SegundasCommit

--- Venda/Venda/obj/Release/net8.0-android/Venda.AssemblyInfo.cs
+++ Venda/Venda/obj/Release/net8.0-android/Venda.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Venda")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+092295f1c46a575255c8a0a280cfc51abfe8dada")]
 [assembly: System.Reflection.AssemblyProductAttribute("Venda")]
